d93e711c51b1595114c8582c347c5eec8f19a36e
PiscoJuan <jpisco@espol.edu.ec>
2023-07-26 17:54:06 -0500

    Cambio de Home a Nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 const routes: Routes = [
   {path: 'home' , component: HomeComponent},
+  {path: 'nosotros' , component: NosotrosComponent},
   // {path: 'contact/:id' , component: ContactDetailComponent}
 ];
 @NgModule({

8f7bf2b707fda6865e25ac20339dec998c273713
PiscoJuan <jpisco@espol.edu.ec>
2023-07-27 20:22:51 -0500

    componenete formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 const routes: Routes = [
   {path: 'home' , component: HomeComponent},
   {path: 'nosotros' , component: NosotrosComponent},
+  {path: 'formulario' , component: FormularioComponent},
   // {path: 'contact/:id' , component: ContactDetailComponent}
 ];
 @NgModule({

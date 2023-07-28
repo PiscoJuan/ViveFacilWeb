1dd1827ac3196c6faf8b8169e9a80df54d7d066d
PiscoJuan <jpisco@espol.edu.ec>
2023-07-27 22:08:29 -0500

    Pop up

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.css';
 // import { Modal } from 'bootstrap';
 
@@ -13,9 +14,14 @@ export class NosotrosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // const element = document.getElementById('myModal') as HTMLElement;
-    // const myModal = new Modal(element);
-    // myModal.show();
+    const modal = document.getElementById('myModal');
+    // Check if the modal element exists and has the 'modal' class
+    if (modal && modal.classList.contains('modal')) {
+        // Create a new Bootstrap Modal instance
+        const bsModal = new bootstrap.Modal(modal);
+        // Show the modal
+        bsModal.show();
+    }
   }
   
 
